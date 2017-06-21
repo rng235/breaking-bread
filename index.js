8d0b2f60dda36d4abe57565f3d6ee5d97d51f954
Ricardo N. Guntur <rng235@nyu.edu>
2017-06-21 13:09:28 -0400

    updated port usage

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var express = require('express');
 var app = express();
 
+
+
+app.set('port', (process.env.PORT || 8000))
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res) {
   res.sendfile('public/html/checkout.html');
 });
 
-app.listen(8000, function () {
+app.listen(app.get('port'), function () {
   console.log('Server running');
 });
 
